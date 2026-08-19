90e86419d53b794b4db41233fa5844d96506e1ab
Ujjwal Vaddepati <ujjwal.vaddepati@vista.com>
2026-08-19 14:06:25 +0530

    Add: python-sorted-copy - submission-0

---
+++ Python Coding Interviews/python-sorted-copy/submission-0.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    return sorted(words)
+    pass
+
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    sorted_numbers = sorted(numbers, key=abs, reverse=True)
+    return sorted_numbers
+    pass
+
+
+# do not modify below this line
+original_words = ["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]
+
+print(original_words)
+print(sort_words(original_words))
+
+original_numbers = [1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]
+
+print(original_numbers)
+print(sort_numbers(original_numbers))
