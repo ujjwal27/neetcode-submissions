076cc7d9f109a7f5a5c9e1cd291cee02baa08478
Ujjwal Vaddepati <ujjwal.vaddepati@vista.com>
2026-09-12 10:37:23 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int NumRescueBoats(int[] people, int limit) {
+        Array.Sort(people);
+
+        int i = 0;
+        int j = people.Length - 1;
+        int result = 0;
+
+        while (i <= j) {
+            if (i == j && people[i] <= limit){
+                result++;
+                return result;
+            }
+
+            if (people[i] + people[j] <= limit) {
+                i++;
+                j--;
+                result++;
+            } else if (people[j] <= limit) {
+                j--;
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
